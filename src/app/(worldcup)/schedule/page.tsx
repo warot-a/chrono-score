7cf5c1eeb0b7dd624642f1a91f710d18b80c3c35
Warot Anusakprasit <tot.anusak@outlook.com>
2026-06-17 20:52:07 +0700

    chor: hide ScheduleHero component in schedule page

--- src/app/(worldcup)/schedule/page.tsx
+++ src/app/(worldcup)/schedule/page.tsx
@@ -1,10 +1,8 @@
-import { ScheduleHero } from '@/components/WorldCup/ScheduleHero';
 import { ScheduleView } from '@/components/WorldCup/ScheduleView';
 
 export default function SchedulePage() {
   return (
     <>
-      <ScheduleHero />
       <main>
         <div className="wrap">
           <ScheduleView />
