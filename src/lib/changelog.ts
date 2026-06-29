6afc84c5f42a90400809f8ad68828119723877fd
Warot Anusakprasit <tot.anusak@outlook.com>
2026-06-29 16:36:31 +0700

    docs(changelog): update latest release notes

--- src/lib/changelog.ts
+++ src/lib/changelog.ts
@@ -2,11 +2,5 @@ import pkg from '../../package.json';
 
 export const CHANGELOG = {
   version: pkg.version,
-  items: [
-    'Live match tracking with real-time score updates',
-    'Real-time scores in standings view',
-    'Free Thai broadcasters for each match',
-    'Updated color scheme from purple to green',
-    'Click matches to search on Google',
-  ],
+  items: ['Fixed incorrect match information in the schedule view.', 'Temporarily hidden the bracket view.'],
 };
